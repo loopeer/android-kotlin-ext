2139f4f279d234f6b9813522e508659821a3a1b3
bytecho <bytecho@163.com>
2017-10-16 19:00:35 +0800

    add textWatcher methods

--- androidext/src/main/java/com/loopeer/androidext/EditText.kt
+++ androidext/src/main/java/com/loopeer/androidext/EditText.kt
@@ -4,7 +4,7 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.widget.EditText
 
-inline fun EditText.afterTextChanged(crossinline afterTextChanged: (String) -> Unit) {
+inline fun EditText.afterTextChanged(crossinline afterTextChanged: (Editable?) -> Unit) {
     this.addTextChangedListener(object : TextWatcher {
         override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
         }
@@ -13,7 +13,35 @@ inline fun EditText.afterTextChanged(crossinline afterTextChanged: (String) -> U
         }
 
         override fun afterTextChanged(editable: Editable?) {
-            afterTextChanged.invoke(editable?.toString() ?: "")
+            afterTextChanged.invoke(editable)
+        }
+    })
+}
+
+inline fun EditText.onTextChanged(crossinline onTextChanged: (CharSequence?, Int, Int, Int) -> Unit) {
+    this.addTextChangedListener(object : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        }
+
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            onTextChanged.invoke(p0, p1, p2, p3)
+        }
+
+        override fun afterTextChanged(editable: Editable?) {
+        }
+    })
+}
+
+inline fun EditText.beforeTextChanged(crossinline beforeTextChanged: (CharSequence?, Int, Int, Int) -> Unit) {
+    this.addTextChangedListener(object : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            beforeTextChanged.invoke(p0, p1, p2, p3)
+        }
+
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        }
+
+        override fun afterTextChanged(editable: Editable?) {
         }
     })
 }
